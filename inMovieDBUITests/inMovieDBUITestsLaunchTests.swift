308bd8728f522e29d6931e3465daf280f4fa27ac
personal <filipe.boeck@hotmail.com>
2023-01-31 22:52:02 -0300

    Initial Commit

---
+++ inMovieDBUITests/inMovieDBUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  inMovieDBUITestsLaunchTests.swift
+//  inMovieDBUITests
+//
+//  Created by Filipe Wolf Boeck on 31/01/23.
+//
+
+import XCTest
+
+final class inMovieDBUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
